be912c4a658a85cf64d4c375d1b6e6459df47db2
JakeAve <jacobjavery@gmail.com>
2022-05-13 21:53:12 -0600

    throw error on exp

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,29 +1,32 @@
-import { djwt } from "../deps.ts";
-import { Env } from "../.env.ts";
+import { djwt } from '../deps.ts';
+import { Env } from '../.env.ts';
 
 const JWT_SECRET = Deno.env.get(Env.JWT_SECRET)!;
 const key = await crypto.subtle.importKey(
-  "jwk",
+  'jwk',
   JSON.parse(JWT_SECRET),
-  { name: "HMAC", hash: "SHA-512" },
+  { name: 'HMAC', hash: 'SHA-512' },
   true,
-  ["sign", "verify"]
+  ['sign', 'verify'],
 );
-const ISS = "survey-app";
+const ISS = 'survey-app';
 
 const { create, verify, decode } = djwt;
 
 export const createJWT = (payload: object) => {
   const exp = new Date().getTime() + 60 * 60 * 1000;
   return create(
-    { alg: "HS512", typ: "JWT" },
+    { alg: 'HS512', typ: 'JWT' },
     { ...payload, exp, iss: ISS },
-    key
+    key,
   );
 };
 
-export const verifyJWT = (token: string) => {
-  return verify(token, key);
+export const verifyJWT = async (token: string) => {
+  const data = await verify(token, key);
+  if (data.iss !== ISS) throw new Error('Invalid issuer');
+  if (data.exp! < new Date().getTime()) throw new Error('Expired token');
+  return data;
 };
 
 export const decodeJWT = (token: string) => decode(token);
